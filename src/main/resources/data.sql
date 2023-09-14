71436ed17029e360c65a0694245f4a018a1c7cf1
dltpwns0 <dltpwns6@naver.com>
2023-09-14 15:00:48 +0900

    SETTING: 초기 데이터를 위한 *.sql 파일 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into coupon_stock
+values (1, 'event coupon', null, 1000,'2023-09-14', '2023-10-14', 10000, 1000, 1000),
+       (2, 'surprise coupon', 10, null,'2023-09-14', '2023-10-14', 5000, 500, 100);
