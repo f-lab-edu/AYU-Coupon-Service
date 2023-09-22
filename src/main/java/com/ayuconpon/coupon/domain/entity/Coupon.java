3dd96c4579bae2d4389c0a2512d6b09ee86c26a1
dltpwns0 <dltpwns6@naver.com>
2023-09-22 21:09:14 +0900

    EDIT: Coupon 엔티티 수정

--- src/main/java/com/ayuconpon/coupon/domain/entity/Coupon.java
+++ src/main/java/com/ayuconpon/coupon/domain/entity/Coupon.java
@@ -12,27 +12,41 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "coupon")
+@Table(
+        name = "coupon",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "coupon_rule_id_and_user_id_unique",
+                        columnNames= {"coupon_rule_id", "user_id"}
+                )
+        }
+)
 public class Coupon extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long couponId;
 
     @Column(name = "user_id")
     private Long userId;
     @ManyToOne
     @JoinColumn(name = "coupon_rule_id")
     private CouponRule couponRule;
+    @Column(name = "issued_at")
+    private LocalDateTime issuedAt;
     @Column(name = "expired_at")
-    private LocalDateTime expireDate;
+    private LocalDateTime expiredAt;
+    @Column(name = "used_at")
+    private LocalDateTime usedAt;
     @Enumerated(EnumType.STRING)
     private Status status;
 
-    public Coupon(Long userId, CouponRule couponRule, LocalDateTime expireDate, Status status) {
+    public Coupon(Long userId, CouponRule couponRule, LocalDateTime issuedAt, LocalDateTime expiredAt, LocalDateTime usedAt, Status status) {
         this.userId = userId;
         this.couponRule = couponRule;
-        this.expireDate = expireDate;
+        this.issuedAt = issuedAt;
+        this.expiredAt = expiredAt;
+        this.usedAt = usedAt;
         this.status = status;
     }
 
