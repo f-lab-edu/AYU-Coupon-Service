f27d9c8db7aece0fb2440df3812d9dc9b2f0d3f6
dltpwns0 <dltpwns6@naver.com>
2023-09-22 20:37:20 +0900

    EDIT: CouponStock 엔티티와 CouponRule 엔티티 통합

--- src/main/java/com/ayuconpon/coupon/domain/entity/CouponRule.java
+++ src/main/java/com/ayuconpon/coupon/domain/entity/CouponRule.java
@@ -5,6 +5,7 @@ import com.ayuconpon.common.Money;
 import com.ayuconpon.common.MoneyConverter;
 import com.ayuconpon.coupon.domain.value.*;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,24 +18,27 @@ public class CouponRule extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long couponRuleId;
 
     @Column(name = "name")
     private String name;
     @Embedded
-    private IssuePeriod issuePeriod;
-    @Embedded
     private DiscountPolicy discountPolicy;
+    @Embedded
+    private Quantity quantity;
+    @Embedded
+    private IssuePeriod issuePeriod;
     @Convert(converter = MoneyConverter.class)
     @Column(name = "min_product_price")
     private Money minProductPrice;
     @Column(name = "usage_hours")
     private Long usageHours;
 
-    public CouponRule(String name, IssuePeriod issuePeriod, DiscountPolicy discountPolicy, Money minProductPrice, Long usageHours) {
+    public CouponRule(String name, DiscountPolicy discountPolicy, Quantity quantity, IssuePeriod issuePeriod, Money minProductPrice, Long usageHours) {
         this.name = name;
-        this.issuePeriod = issuePeriod;
         this.discountPolicy = discountPolicy;
+        this.quantity = quantity;
+        this.issuePeriod = issuePeriod;
         this.minProductPrice = minProductPrice;
         this.usageHours = usageHours;
     }
