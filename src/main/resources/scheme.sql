fb9ba37a9eabd1780b64e9b61a2a26948c0750b8
dltpwns0 <dltpwns6@naver.com>
2023-09-24 00:10:04 +0900

    EDIT: 테이블 변경에 따라 *.sql 파일 변경

--- src/main/resources/scheme.sql
+++ src/main/resources/scheme.sql
@@ -1,57 +1,44 @@
-DROP TABLE IF EXISTS `coupon`, `coupon_stock`, `coupon_rule`;
-
--- -----------------------------------------------------
--- Table `coupon_rule`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `coupon_rule` (
-    `id` BIGINT NOT NULL,
-    `name` VARCHAR(45) NULL,
-    `discount_type` VARCHAR(45) NOT NULL,
-    `discount_rate` INT NULL,
-    `discount_value` INT NULL,
-    `started_at` DATETIME NOT NULL,
-    `finished_at` DATETIME NOT NULL,
-    `usage_hours` INT NOT NULL,
-    `min_product_price` INT NOT NULL,
-    PRIMARY KEY (`id`))
-    ENGINE = InnoDB;
-
--- -----------------------------------------------------
--- Table `coupon_stock`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `coupon_stock` (
-    `id` BIGINT NOT NULL,
-    `coupon_rule_id` BIGINT NOT NULL,
-    `total_quantity` INT NOT NULL,
-    `left_quantity` INT NOT NULL,
-    PRIMARY KEY (`id`),
-    INDEX `fk_coupon_id_idx` (`coupon_rule_id` ASC) VISIBLE,
-    CONSTRAINT `fk_coupon_rule_coupon_stock`
-    FOREIGN KEY (`coupon_rule_id`)
-    REFERENCES `coupon_db`.`coupon_rule` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-    ENGINE = InnoDB;
+DROP TABLE IF EXISTS `coupon`, `user_coupon`;
 
 -- -----------------------------------------------------
 -- Table `coupon`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `coupon` (
-    `id` BIGINT NOT NULL,
+    `coupon_id` BIGINT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(45) NULL,
+    `discount_type` VARCHAR(45) NOT NULL,
+    `discount_rate` INT NULL,
+    `discount_value` INT NULL,
+    `total_quantity` INT NOT NULL,
+    `left_quantity` INT NOT NULL,
+    `started_at` DATETIME NOT NULL,
+    `finished_at` DATETIME NOT NULL,
+    `usage_hours` INT NOT NULL,
+    `min_product_price` INT NOT NULL,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`coupon_id`))
+    ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `user_coupon`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `user_coupon` (
+    `user_coupon_id` BIGINT NOT NULL AUTO_INCREMENT,
+    `coupon_id` BIGINT NOT NULL,
     `user_id` BIGINT NOT NULL,
-    `coupon_stock_id` BIGINT NOT NULL,
-    `coupon_rule_id` BIGINT NOT NULL,
     `issued_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `expired_at` DATETIME NOT NULL,
     `used_at` DATETIME NULL,
-    `status` VARCHAR(45) NOT NULL,
-    PRIMARY KEY (`id`),
-    INDEX `fk_coupon_rule_id_idx` (`coupon_rule_id` ASC) VISIBLE,
-    INDEX `fk_coupon_stock_id_idx` (`coupon_stock_id` ASC) VISIBLE,
-    CONSTRAINT `fk_coupon_rule_coupon`
-        FOREIGN KEY (`coupon_rule_id`)
-            REFERENCES `coupon_db`.`coupon_rule` (`id`),
-    CONSTRAINT `fk_coupon_stock_coupon`
-        FOREIGN KEY (`coupon_stock_id`)
-            REFERENCES `coupon_db`.`coupon_stock` (`id`))
+    `status` VARCHAR(50) NOT NULL,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`user_coupon_id`),
+    INDEX `fk_coupon_id_idx` (`coupon_id` ASC) VISIBLE,
+    CONSTRAINT `fk_coupon_coupon`
+        FOREIGN KEY (`coupon_id`)
+            REFERENCES `coupon` (`coupon_id`))
     ENGINE = InnoDB;
+
+ALTER TABLE user_coupon
+    ADD CONSTRAINT user_coupon_id_and_user_id_unique UNIQUE(coupon_id, user_id);
