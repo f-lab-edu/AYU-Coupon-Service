f40a1b8c4e063b87d3ed6776def40ae567e151fb
DongJun-Lee <dongjun.dev@gmail.com>
2023-09-24 00:14:00 +0900

    EDIT: 쿠폰 테이블 변경에 따라 *.sql 파일 변경 #19

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,3 @@
-insert into coupon_rule
-values (1, 'event coupon','FIX_DISCOUNT', null, 1000,'2023-09-14', '2023-10-14', 24, 10000),
-       (2, 'surprise coupon', 'RATE_DISCOUNT', 0.1, null, '2023-09-14', '2023-10-14', 24, 5000);
-
-insert into coupon_stock
-values (1, 1, 1000, 1000),
-       (2, 2, 1000, 1000);
+insert into coupon
+values (null, 'event fix coupon','FIX_DISCOUNT', null, 1000, 1000, 1000,'2023-09-14', '2023-10-14', 24, 10000, '2023-09-14', '2023-09-14'),
+       (null, 'event rate coupon','RATE_DISCOUNT', 0.1, null, 1000, 1000,'2023-09-14', '2023-10-14', 24, 10000, '2023-09-14', '2023-09-14');
