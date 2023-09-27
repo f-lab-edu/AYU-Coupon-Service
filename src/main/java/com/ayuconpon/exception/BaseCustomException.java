4f5f8d9774e2264210c65215cd726cf2e3c71f2f
dltpwns0 <dltpwns6@naver.com>
2023-09-27 21:26:44 +0900

    BaseCustomException 에외 종류 변경

--- src/main/java/com/ayuconpon/exception/BaseCustomException.java
+++ src/main/java/com/ayuconpon/exception/BaseCustomException.java
@@ -2,7 +2,7 @@ package com.ayuconpon.exception;
 
 import org.springframework.http.HttpStatus;
 
-public abstract class BaseCustomException extends Exception {
+public abstract class BaseCustomException extends RuntimeException {
 
     public abstract HttpStatus getStatus();
 
