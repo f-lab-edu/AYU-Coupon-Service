7e32906e857d6d77dee1ead806c2ec78a7345ee3
DongJun-Lee <dongjun.dev@gmail.com>
2023-09-29 17:08:46 +0900

    API 예외 응답 기능 고도화를 위한 예외 클래스 개발 (#31)
    * FEAT: API 예외 응답 클래스 개발
    
    * EDIT: 커스텀 예외처리 필드 정적 타입으로 변경
    
    * BaseCustomException 에외 종류 변경

---
+++ src/main/java/com/ayuconpon/exception/BaseCustomException.java
@@ -0,0 +1,11 @@
+package com.ayuconpon.exception;
+
+import org.springframework.http.HttpStatus;
+
+public abstract class BaseCustomException extends RuntimeException {
+
+    public abstract HttpStatus getStatus();
+
+    public abstract String getMessage();
+
+}
