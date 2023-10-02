8384799b0c033d51b9c323d1418e02b3aada0215
DongJun-Lee <dongjun.dev@gmail.com>
2023-10-03 01:06:28 +0900

    쿠폰 발급 API 컨트롤러에 서비스 코드 추가 (#47)

--- src/main/java/com/ayuconpon/coupon/controller/CouponController.java
+++ src/main/java/com/ayuconpon/coupon/controller/CouponController.java
@@ -1,23 +1,30 @@
 package com.ayuconpon.coupon.controller;
 import com.ayuconpon.coupon.controller.request.IssueCouponRequest;
 import com.ayuconpon.coupon.controller.response.IssueCouponResponse;
+import com.ayuconpon.coupon.service.IssueCouponCommand;
+import com.ayuconpon.coupon.service.IssueCouponService;
 import com.ayuconpon.resolver.UserId;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class CouponController {
+
+    private final IssueCouponService issueCouponService;
 
     @PostMapping("/users/coupons")
     public ResponseEntity<IssueCouponResponse> issueCoupon(
             @UserId Long userId,
             @Valid @RequestBody IssueCouponRequest issueCouponRequest) {
 
-        // TODO 쿠폰 서비스 개발
+        IssueCouponCommand command = new IssueCouponCommand(userId, issueCouponRequest.getCouponId());
+        Long issuedCouponId = issueCouponService.issue(command);
 
-        return ResponseEntity.ok(new IssueCouponResponse(1L));
+        return ResponseEntity.ok(new IssueCouponResponse(issuedCouponId));
     }
 
 }
