e812ea2b005daee03298062d47ff929977869138
DongJun-Lee <dongjun.dev@gmail.com>
2023-10-03 00:16:09 +0900

    쿠폰 발급 응답 형식 변경 (#44)

--- src/main/java/com/ayuconpon/coupon/controller/CouponController.java
+++ src/main/java/com/ayuconpon/coupon/controller/CouponController.java
@@ -1,9 +1,9 @@
 package com.ayuconpon.coupon.controller;
-import com.ayuconpon.common.ApiResponse;
 import com.ayuconpon.coupon.controller.request.IssueCouponRequest;
 import com.ayuconpon.coupon.controller.response.IssueCouponResponse;
 import com.ayuconpon.resolver.UserId;
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class CouponController {
 
     @PostMapping("/api/coupons")
-    public ApiResponse<IssueCouponResponse> issueCoupon(
+    public ResponseEntity<IssueCouponResponse> issueCoupon(
             @UserId Long userId,
             @Valid @RequestBody IssueCouponRequest issueCouponRequest) {
 
         // TODO 쿠폰 서비스 개발
 
-        return ApiResponse.ok(new IssueCouponResponse(1L));
+        return ResponseEntity.ok(new IssueCouponResponse(1L));
     }
 
 }
--- src/main/java/com/ayuconpon/handler/CouponExceptionHandler.java
+++ src/main/java/com/ayuconpon/handler/CouponExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.ayuconpon.handler;
 
-import com.ayuconpon.common.ApiResponse;
 import com.ayuconpon.exception.BaseCustomException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,23 +11,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class CouponExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ApiResponse<String> handle(MethodArgumentNotValidException e) {
-        return ApiResponse.error(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+    public ResponseEntity<String> handle(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(e.getStatusCode())
+                .body(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ApiResponse<String> handle(IllegalArgumentException e) {
-        return ApiResponse.error(e.getMessage());
+    public ResponseEntity<String> handle(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
     }
 
     @ExceptionHandler(IllegalStateException.class)
-    public ApiResponse<String> handle(IllegalStateException e) {
-        return ApiResponse.error(e.getMessage());
+    public ResponseEntity<String> handle(IllegalStateException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
     }
     
     @ExceptionHandler(BaseCustomException.class)
-    public ApiResponse<String> handle(BaseCustomException e) {
-        return ApiResponse.error(e);
+    public ResponseEntity<String> handle(BaseCustomException e) {
+        return ResponseEntity.status(e.getStatus())
+                .body(e.getMessage());
     }
 
 }
