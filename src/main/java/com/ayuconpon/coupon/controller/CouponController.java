2e72c67ab1ef6698b60407abe7060566aa92edd3
dltpwns0 <dltpwns6@naver.com>
2023-10-11 00:39:55 +0900

    FEAT: 전체 쿠폰 조회 서비스 개발

--- src/main/java/com/ayuconpon/coupon/controller/CouponController.java
+++ src/main/java/com/ayuconpon/coupon/controller/CouponController.java
@@ -5,11 +5,11 @@ import com.ayuconpon.coupon.controller.response.ShowCouponsResponse;
 import com.ayuconpon.coupon.service.ShowCouponsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -20,7 +20,7 @@ public class CouponController {
 
     @GetMapping("/coupons")
     public ResponseEntity<ShowCouponsResponse> showCoupons(Pageable pageable) {
-        List<CouponDto> couponDtos = showCouponsService.getCoupons(pageable);
+        List<CouponDto> couponDtos = showCouponsService.getCouponsInProgress(LocalDateTime.now(),pageable);
         ShowCouponsResponse showCouponsResponse = ShowCouponsResponse.from(couponDtos);
         return ResponseEntity.ok(showCouponsResponse);
     }
