1c2611f7bb29ebb649bf5ace62b393d530ccf3c7
dltpwns0 <dltpwns6@naver.com>
2023-10-16 20:35:16 +0900

    data.sql 파일 변경
    - 테스트 코드가 현재 발행 날짜 데이터에 의존적이어서 테스트가 실패함
    - 이후 모든 테스트 코드를 데이터베이스에 의존적이지 않도록 코드 수정 예정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 insert into coupon(coupon_id, name, discount_type, discount_rate, discount_price, total_quantity, left_quantity, started_at, finished_at, usage_hours, min_product_price)
-values (1, 'event fix coupon A','FIX_DISCOUNT', null, 1000, 1000, 1000,'2023-09-14', '2023-10-14', 24, 10000),
-       (2, 'event fix coupon B','FIX_DISCOUNT', null, 1000, 1000, 1000,'2023-09-14', '2023-10-14', 24, 10000),
-       (3, 'event rate coupon C','RATE_DISCOUNT', 0.1, null, 1000, 1000,'2023-09-14', '2023-10-14', 24, 10000),
-       (4, 'event rate coupon D','RATE_DISCOUNT', 0.1, null, 1000, 1000,'2023-09-14', '2023-10-14', 24, 10000);
+values (1, 'event fix coupon A','FIX_DISCOUNT', null, 1000, 1000, 1000,'2023-09-14', '2024-10-14', 24, 10000),
+       (2, 'event fix coupon B','FIX_DISCOUNT', null, 1000, 1000, 1000,'2023-09-14', '2024-10-14', 24, 10000),
+       (3, 'event rate coupon C','RATE_DISCOUNT', 0.1, null, 1000, 1000,'2023-09-14', '2024-10-14', 24, 10000),
+       (4, 'event rate coupon D','RATE_DISCOUNT', 0.1, null, 1000, 1000,'2023-09-14', '2024-10-14', 24, 10000);
 
 
 insert into user(user_id, name, status)
