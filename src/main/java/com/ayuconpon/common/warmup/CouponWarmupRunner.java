03575155568c98b886b081f6543dbb54630e0b4f
dltpwns0 <dltpwns6@naver.com>
2023-10-19 01:25:24 +0900

    EDIT: warm up 코드 수정

--- src/main/java/com/ayuconpon/common/warmup/CouponWarmupRunner.java
+++ src/main/java/com/ayuconpon/common/warmup/CouponWarmupRunner.java
@@ -1,14 +1,12 @@
 package com.ayuconpon.common.warmup;
 
-import com.ayuconpon.coupon.controller.CouponController;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.stream.IntStream;
 
@@ -16,21 +14,21 @@ import java.util.stream.IntStream;
 @RequiredArgsConstructor
 public class CouponWarmupRunner {
 
-    private final CouponController couponController;
-
-    private final Logger log = LoggerFactory.getLogger(CouponWarmupRunner.class);
+    private final static Logger log = LoggerFactory.getLogger(CouponWarmupRunner.class);
+    private static final RestTemplate restTemplate = new RestTemplate();
     private static final Integer WARM_UP_COUNT = 5000;
+    private static final String COUPONS_URL = "http://localhost:8080/v1/coupons";
+
 
     @EventListener(ApplicationReadyEvent.class)
     public void warmup() {
-        Pageable pageable = PageRequest.of(0, 10);
         log.info("start Coupon warm up");
         try {
             IntStream.rangeClosed(1, WARM_UP_COUNT).forEach(i -> {
-                couponController.showCoupons(pageable);
+                restTemplate.getForObject(COUPONS_URL, String.class);
             });
         } catch (Exception e) {
-            log.warn("UserCouponWarmupRunner caught exception at inner. ex {}", e.getMessage());
+            log.warn("CouponWarmupRunner caught exception at inner. ex {}", e.getMessage());
         }
         log.info("finish Coupon warm up");
     }
--- src/main/java/com/ayuconpon/common/warmup/UserCouponWarmupRunner.java
+++ src/main/java/com/ayuconpon/common/warmup/UserCouponWarmupRunner.java
@@ -1,35 +1,37 @@
 package com.ayuconpon.common.warmup;
 
-import com.ayuconpon.common.exception.AlreadyUsedUserCouponException;
-import com.ayuconpon.common.exception.DuplicatedCouponException;
-import com.ayuconpon.usercoupon.controller.UserCouponController;
-import com.ayuconpon.usercoupon.controller.request.IssueUserCouponRequest;
-import com.ayuconpon.usercoupon.controller.request.UseUserCouponRequest;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.stream.IntStream;
 
 @Component
-@RequiredArgsConstructor
 public class UserCouponWarmupRunner {
 
-    private final UserCouponController userCouponController;
+    private static final RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
+    private static final Logger log = LoggerFactory.getLogger(UserCouponWarmupRunner.class);
 
-    private final Logger log = LoggerFactory.getLogger(UserCouponWarmupRunner.class);
     private static final Long ADMIN_USER_ID = 1L;
     private static final Integer WARM_UP_COUNT = 5000;
-    private static final Long REQUEST_COUPON_ID = 1L;
-    private static final Long REQUEST_PRODUCT_PRICE = 10000L;
+
+    private static final String USER_COUPONS_URL = "http://localhost:8080/v1/users/me/user-coupons";
+    private static final String USE_USER_COUPONS_URL = "http://localhost:8080/v1/users/me/user-coupons/1";
+    private static final String USE_COUPON_BODY = "{ \"productPrice\" : \"10000\" }";
+    private static final String ISSUE_COUPON_BODY = "{ \"couponId\" : \"1\" }";
+
 
     @EventListener(ApplicationReadyEvent.class)
     public void warmup() {
+
         log.info("start UserCoupon warm up");
         try {
             IntStream.rangeClosed(1, WARM_UP_COUNT).forEach(i -> {
@@ -44,20 +46,33 @@ public class UserCouponWarmupRunner {
     }
 
     private void warmupShowUserCoupons() {
-        Pageable pageable = PageRequest.of(0, 10);
-        userCouponController.showUserCoupons(ADMIN_USER_ID, pageable);
+        HttpEntity<String> request = new HttpEntity<>(getHeaders());
+        restTemplate.exchange(USER_COUPONS_URL, HttpMethod.GET, request, String.class);
     }
 
     private void warmupIssueCoupon() {
+        HttpHeaders headers = getHeaders();
+        HttpEntity<String> request = new HttpEntity<>(ISSUE_COUPON_BODY, headers);
         try {
-            userCouponController.issueCoupon(ADMIN_USER_ID, new IssueUserCouponRequest(REQUEST_COUPON_ID));
-        } catch (DuplicatedCouponException ignored) {}
+            restTemplate.exchange(USER_COUPONS_URL, HttpMethod.POST, request, String.class);
+        } catch (HttpClientErrorException ignore) {}
     }
 
     private void warmupUseCoupon() {
+        HttpHeaders headers = getHeaders();
+        HttpEntity<String> request = new HttpEntity<>(USE_COUPON_BODY, headers);
         try {
-            userCouponController.useCoupon(ADMIN_USER_ID, REQUEST_COUPON_ID,new UseUserCouponRequest(REQUEST_PRODUCT_PRICE));
-        } catch (AlreadyUsedUserCouponException ignored) {}
+            restTemplate.exchange(USE_USER_COUPONS_URL, HttpMethod.PATCH, request, String.class);
+        } catch (HttpClientErrorException ignore) {}
+    }
+
+    private HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        headers.add("Accept", "application/json");
+        headers.add("Accept-Encoding", "gzip, deflate, br");
+        headers.add("User-Id", ADMIN_USER_ID.toString());
+        return headers;
     }
 
 }
