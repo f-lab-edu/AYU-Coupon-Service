e045723338023095ad06c98ae19f8ea17f52e9f4
dltpwns0 <dltpwns6@naver.com>
2023-10-25 01:52:13 +0900

    FEAT : JVM Warmup을 병렬 처리하도록 리팩토링

--- src/main/java/com/ayuconpon/common/config/warmup/WarmupConfig.java
+++ src/main/java/com/ayuconpon/common/config/warmup/WarmupConfig.java
@@ -1,11 +1,14 @@
 package com.ayuconpon.common.config.warmup;
 
-import com.ayuconpon.warmup.CouponWarmupRunner;
-import com.ayuconpon.warmup.UserCouponWarmupRunner;
+import com.ayuconpon.warmup.WarmupRegistry;
+import com.ayuconpon.warmup.WarmupRunner;
+import com.ayuconpon.warmup.WarmupStarter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 @EnableConfigurationProperties({CouponWarmupProperties.class, UserCouponWarmupProperties.class})
@@ -16,13 +19,57 @@ public class WarmupConfig {
     private final UserCouponWarmupProperties userCouponWarmupProperties;
 
     @Bean
-    public CouponWarmupRunner couponWarmupRunner() {
-        return new CouponWarmupRunner(couponWarmupProperties);
+    public WarmupRunner showCouponsWarmupRunner() {
+        return WarmupRunner
+                .create(HttpMethod.GET, couponWarmupProperties.getBaseUrl(), couponWarmupProperties.getWarmupCount());
     }
 
     @Bean
-    public UserCouponWarmupRunner userCouponWarmupRunner() {
-        return new UserCouponWarmupRunner(userCouponWarmupProperties);
+    public WarmupRunner showUserCouponsWarmupRunner() {
+        return WarmupRunner
+                .create(HttpMethod.GET, userCouponWarmupProperties.getBaseUrl(), userCouponWarmupProperties.getWarmupCount())
+                .headers(getHeaders());
+    }
+
+    @Bean
+    public WarmupRunner issueCouponWarmupRunner() {
+        return WarmupRunner
+                .create(HttpMethod.POST, userCouponWarmupProperties.getBaseUrl(), userCouponWarmupProperties.getWarmupCount())
+                .headers(getHeaders())
+                .bodyValue(userCouponWarmupProperties.getIssueApiBody());
+    }
+
+    @Bean
+    public WarmupRunner useCouponWarmupRunner() {
+        return WarmupRunner
+                .create(HttpMethod.PATCH, userCouponWarmupProperties.getBaseUrl(), userCouponWarmupProperties.getWarmupCount())
+                .headers(getHeaders())
+                .pathSegment(userCouponWarmupProperties.getUseApiPathSegment().toString())
+                .bodyValue(userCouponWarmupProperties.getUseApiBody());
+    }
+
+    @Bean
+    public WarmupRegistry warmupRegistry() {
+        WarmupRegistry registry = new WarmupRegistry();
+        registry.addWarmupRunner(showCouponsWarmupRunner())
+                .addWarmupRunner(showUserCouponsWarmupRunner())
+                .addWarmupRunner(issueCouponWarmupRunner())
+                .addWarmupRunner(useCouponWarmupRunner());
+        return registry;
+    }
+
+    @Bean
+    public WarmupStarter warmupStarter() {
+        return new WarmupStarter(warmupRegistry());
+    }
+
+    private HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        headers.add("Accept", "application/json");
+        headers.add("Accept-Encoding", "gzip, deflate, br");
+        headers.add("User-Id", userCouponWarmupProperties.getUserid().toString());
+        return headers;
     }
 
 }
---
+++ src/main/java/com/ayuconpon/warmup/WarmupRegistry.java
@@ -0,0 +1,20 @@
+package com.ayuconpon.warmup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WarmupRegistry {
+
+    private final List<WarmupRunner> registrations = new ArrayList<>();
+
+    public WarmupRegistry addWarmupRunner(WarmupRunner warmupRunner) {
+        this.registrations.add(warmupRunner);
+        return this;
+    }
+
+    public List<WarmupRunner> getWarmupRunners() {
+        return this.registrations.stream()
+                .toList();
+    }
+
+}
