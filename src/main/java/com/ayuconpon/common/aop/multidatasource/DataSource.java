8cabbf80f89cecf79523e574245c37693a5bc981
dltpwns0 <dltpwns6@naver.com>
2023-10-28 17:01:31 +0900

    FEAT: 어노테이션 기반 multi datasource routing 개발

---
+++ src/main/java/com/ayuconpon/common/aop/multidatasource/DataSource.java
@@ -0,0 +1,17 @@
+package com.ayuconpon.common.aop.multidatasource;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DataSource {
+
+    /**
+     * @return datasource name
+     */
+    String value();
+
+}
