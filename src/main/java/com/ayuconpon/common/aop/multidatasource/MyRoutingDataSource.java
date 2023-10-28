9d7ac2f1da2539d31a885e18d713668b7eccc5ff
dltpwns0 <dltpwns6@naver.com>
2023-10-28 17:01:31 +0900

    FEAT: 어노테이션 기반 multi datasource routing 개발 (#99)

---
+++ src/main/java/com/ayuconpon/common/aop/multidatasource/MyRoutingDataSource.java
@@ -0,0 +1,11 @@
+package com.ayuconpon.common.aop.multidatasource;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+public class MyRoutingDataSource extends AbstractRoutingDataSource {
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return DataSourceNameContextHolder.getDataSourceName();
+    }
+
+}
