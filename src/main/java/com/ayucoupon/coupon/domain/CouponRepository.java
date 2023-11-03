19308498c7760429fb154a7b67377f0632f08b3c
dltpwns0 <dltpwns6@naver.com>
2023-11-03 20:53:47 +0900

    EDIT: 사용자 쿠폰 발급 로직 개선

--- src/main/java/com/ayucoupon/coupon/domain/CouponRepository.java
+++ src/main/java/com/ayucoupon/coupon/domain/CouponRepository.java
@@ -5,7 +5,10 @@ import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,6 +18,9 @@ public interface CouponRepository extends JpaRepository<Coupon, Long> {
     @Lock(value = LockModeType.PESSIMISTIC_WRITE)
     @Query("select c from Coupon c where c.couponId=:couponId")
     Coupon findByIdWithPessimisticLock(Long couponId);
+
+    @Query("select c.usageHours from Coupon c where c.couponId=:couponId")
+    Long findCouponUsageHours(Long couponId);
 
     @Query("select c from Coupon c " +
             "where c.issuePeriod.startedAt <= :currentTime and :currentTime <= c.issuePeriod.finishedAt")
